ef656fad005b7afe3b5e27b74067c0515e9520ae
John Salvador <salvador.johnc@gmail.com>
2014-03-14 14:45:17 -0400

    Updated server

--- connect.php
+++ connect.php
@@ -1,30 +1,10 @@
 <?php
-$http_client_ip;
-$http_x_forwarded_for;
-$remote_addr = $_SERVER;
-if(isset($_SERVER['HTTP_CLIENT_IP']))
-	$http_client_ip = $_SERVER['HTTP_CLIENT_IP'];
-if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-	$http_x_forwarded_for = $_SERVER['HTTP_X_FORWARDED_FOR'];
-if(isset($_SERVER['REMOTE_ADDR']))
-	$remote_addr = $_SERVER['REMOTE_ADDR'];
+include '../config.php';
 
-if(!empty($http_client_ip)){
-	$ip_address = $http_client_ip;	
-} else if(!empty($http_x_forwarded_for)){
-	$ip_address = $http_x_forwarded_for;
-} else {
-	$ip_address = $remote_addr;
-}
-
-if($ip_address=='::1'){
-	$dbServer = "localhost";
-}else{
-	$dbServer = "sql2.njit.edu";
-}
-$dbuserName = "jbs44";
-$dbpassword = "";
-$dbName = "jbs44";
+$dbServer = DB_HOST;
+$dbuserName = DB_USER;
+$dbpassword = DB_PASSWORD;
+$dbName = DB_DATABASE;
 
 // ********** Connect. Change fields above.
 // ********** Do not edit below

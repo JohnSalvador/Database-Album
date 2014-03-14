096cbab7627fa4610dfa853e9d22ab3aedbe6f65
John Salvador <salvador.johnc@gmail.com>
2014-03-14 15:20:55 -0400

    Hotfix

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require_once 'connect.php';
     	<h1>Welcome to the CD Manager</h1>
     </div>
     <?php
-	if(isset($_COOKIE['username'])&&$_COOKIE['username']=='Gehani'){
+	$_COOKIE['username']=='Gehani';
 	include 'header.php';
 	?>
     <div id="indexcontent">
@@ -24,28 +24,7 @@ require_once 'connect.php';
         	<li>John</li>
         <ul>
     </div>
-    <?php
-	} else{
-	?>
-    <div id="indexlogin">
-    	Please Log in:
-        <?php
-		if(isset($_POST['username'])&&isset($_POST['password'])){
-			$username=$_POST['username'];
-			$password=$_POST['password'];
-			if(!empty($username)&&!empty($password)){
-				echo 'Logging in...';
-				
-				if($username=='Gehani'&&$password='database'){
-					$allow=1;
-					setcookie('username','Gehani',time()+4800);
-					header("Location: index.php");
-				}
-			}else{
-				echo '<h3>Error Logging in: Please enter your username and password</h3>';	
-			}
-		}
-		?>
+   
         <form action="index.php" id="login_form" method="POST">
             <input type="text" name="username" value="" placeholder="Username"/><br />
             <input type="password" name="password" value="" placeholder="Password"/> <br />
@@ -53,8 +32,5 @@ require_once 'connect.php';
         </form>
     </div>
     
-    <?php
-	}
-	?>
 </body>
 </html>
